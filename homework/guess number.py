f7528ad0d468fce102b9b9354251fed30bd1cc51
StoneJiaJia <huyou1130@163.com>
2015-03-28 17:09:38 +0800

    Update guess number.py

--- homework/guess number.py
+++ homework/guess number.py
@@ -1,22 +1,28 @@
-
 import math
 import simplegui
 message = "welcome!"
 import random
-t = 60
+t = 600
 interval = 100
 secret_number = 0
-value = 0
+value = "0"
 num_range = 0
-number_guess = 7
+left = 7
 # helper function to start and restart the game
 def new_game():
-    t == 60
-    if num_range == 100:
-        range100()
-    elif num_range == 1000:
-        range1000()
+    global t,message,interval,secrect_number,value,left
+    t = 600
+    interval = 100
+    secret_number = 0
+    value = "0"
+    num_range = 0
+    left = 7
+    message = "welcome!"
 
+    
+    
+   
+    
 # global variables
 
 # in tenths of seconds into formatted string A:BC.D
@@ -33,71 +39,72 @@ def range100():
     global message
     global secret_number
     global number_guess
-    number_guess = 7
+    global left
+    left = 7
     secret_number = random.randrange(0, 100)
     message = "Guess the number!"
     global t
-    t = 60
+    t = 600
     timer.start() 
+    new_game
+    print  secret_number
     
 def range1000():
     global message
     global secret_number
     global number_guess
+    global left
     num_range = 1000
-    number_guess = 10
+    left = 10
     secret_number = random.randrange(0,1000)
     message = "Guess the number!"
     global t
-    t = 60
+    t = 600
     timer.start() 
-def input_guess(guess):
-    global value
-    value = int(guess)
-def outcome(a):
-    result = str(a)
-    return result
-def number_guess():
+
     
-    if range100:
-        number_guess == 7
-    if range1000:
-        number_guess == 10
-        return number_guess
+def number_guess(val):
+    
+    global message,value,left
+    value = int (val)
+    
+
+    
 # Convert to strings
 
 # define event handlers for buttons 
 
 # mian game logic goes here
+    
+    
 
-    if str.isdigit(outcome):
-        global message
-        message = "you guess is"
-
-    if int(outcome) == secret_number:
+    if  value == secret_number:
+        
         
         message = "You are right"
         new_game()
 
-    if int(outcome) > secret_number:
-        number_guess = number_guess - 1
-        
+    elif value > secret_number:
+        left = left -1
         message = "Lower"
-        if number_guess == 0:
-            message = "lose"
+        
+        
 
-    if int(outcome) < secret_number:
-        number_guess = number_guess - 1
-        message = "Higher!"
-        if number_guess == 0:
-            message = "You lose"
-            new_game()
+    elif value < secret_number:
        
+        left = left - 1
+        
+        message = "Higher!"
+  
+    
             
     else:
-        message = "Invalid entry. Please enter an in-range number"
-
+        message = "Invalid entry"
+    if left == 0:
+        message = "You lose"
+        new_game()
         
+    
         
 # define event handler for timer with 0.1 sec interval
 def tick():
@@ -111,16 +118,17 @@ def tick():
 
 # define draw handler
 def draw(canvas):
+    global value
     text = format(t)
     number = number_guess
    
  
     
-    canvas.draw_text(outcome(value), (125, 125), 45, "pink")
-    canvas.draw_text( str(text), (145, 40), 42, "white")
-    canvas.draw_text("left"+str(number_guess), (30,40), 42, "white")
+    canvas.draw_text(str(value), (250, 250), 50, "pink")
+    canvas.draw_text( str(text), (145, 100),20, "white")
+    canvas.draw_text("left"+str(left), (30,40), 20, "white")
     canvas.draw_text(message, [50,70], 24, "Red")
-   
+    
         
         
         
@@ -129,12 +137,13 @@ def draw(canvas):
         
             
 # creat frame 
-frame = simplegui.create_frame("Guess the number", 250,250,)
+frame = simplegui.create_frame("Guess the number", 500,500,)
     
 # register event handlers for control elements and start frame
 frame.add_button("Range: [0,100)", range100, 200)
 frame.add_button("Range: [0,1000)", range1000, 200)
-frame.add_input("Enter a guess", input_guess, 200)
+frame.add_input("Enter a guess", number_guess, 200)
+
 frame.set_draw_handler(draw)
 timer = simplegui.create_timer(interval, tick)
 # call new_game 
